41b435d048e3f3caf914ad6ef54c2fb5d86740e6
Miroshnichenko Igor <Miroshnichenko.Igor.Valerivich@yandex.ru>
2022-06-15 21:09:04 +0500

    Initial commit

---
+++ src/main/java/ru/miroshka/apsoft/ApSoftApplication.java
@@ -0,0 +1,13 @@
+package ru.miroshka.apsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApSoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/miroshka/apsoft/ApSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.miroshka.apsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApSoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
